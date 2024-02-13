5d5113ff38e3e1e52e6191031a145685e97b4160
zor009 <anzohra@outlook.com>
2024-02-13 12:34:43 -0500

    simple react vite typescript features

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,7 @@
+//PascalCasing the letter is capitalized of the naemof function
+function Message (){
+    const name = 'Zohra'
+    //JSX: JavaScript XML 
+    return <h1>Hello {name}</h1>;
+}
+export default Message;
