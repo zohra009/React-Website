d28a92858cd2ecacf2aab33320d9257804b16f1b
zor009 <anzohra@outlook.com>
2024-02-14 16:02:47 -0500

    refactor

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -3,22 +3,24 @@
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
-  if (items.length === 0) {
-    // return (
-    //   //empty bracets are <framgemt> that wraps our html of react components
-    //   <>
-    //     <h1>List</h1>
-    //     <p>No items found</p>
-    //   </>
-    // );
-  }
+  const message = items.length === 0 ? <p>Not item found</p> : null; //if this message is true return <p> msg otherwise render nothing
+
+  //if (items.length === 0) {
+  // return (
+  //   //empty bracets are <framgemt> that wraps our html of react components
+  //   <>
+  //     <h1>List</h1>
+  //     <p>No items found</p>
+  //   </>
+  // );
+  // }
+
   return (
     //empty 'fragment' is included - that's how react reads the empty braces <framgment> sames ad using <> and </>
-    //in this JSX we cannot use conditional expression except when we use { } and turenary statement
+    //in this JSX we cannot use conditional expression except when we use { } and ternary statement
     <>
       <h1>Lists</h1>
-      {items.length === 0 ? <p>Not item found</p> : null}{" "}
-      {/* if this message is true return <p> msg otherwise render nothing */}
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
