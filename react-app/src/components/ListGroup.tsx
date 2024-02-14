dbdcdd255bf19adf5ffa8e301e50e9981711a94c
zor009 <anzohra@outlook.com>
2024-02-14 16:10:05 -0500

    refactor ternary

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -3,24 +3,18 @@
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
-  const message = items.length === 0 ? <p>Not item found</p> : null; //if this message is true return <p> msg otherwise render nothing
 
-  //if (items.length === 0) {
-  // return (
-  //   //empty bracets are <framgemt> that wraps our html of react components
-  //   <>
-  //     <h1>List</h1>
-  //     <p>No items found</p>
-  //   </>
-  // );
-  // }
+  const getMessage = () => {
+    return items.length === 0 ? <p>Not item found</p> : null; //if this message is true return <p> msg otherwise render nothing
+  };
 
   return (
     //empty 'fragment' is included - that's how react reads the empty braces <framgment> sames ad using <> and </>
     //in this JSX we cannot use conditional expression except when we use { } and ternary statement
     <>
       <h1>Lists</h1>
-      {message}
+      {/* {getMessage()} */}
+      {items.length === 0 && <p>Not item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
