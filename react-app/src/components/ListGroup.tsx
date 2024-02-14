55aa166082fa858e11d2c7513f14dafbee24d500
zor009 <anzohra@outlook.com>
2024-02-14 10:48:48 -0500

    added ternary operator

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -4,12 +4,21 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
   if (items.length === 0) {
-    return <p>No items found</p>;
+    // return (
+    //   //empty bracets are <framgemt> that wraps our html of react components
+    //   <>
+    //     <h1>List</h1>
+    //     <p>No items found</p>
+    //   </>
+    // );
   }
   return (
-    //empty 'fragment' is included - that's how react reads the empty braces
+    //empty 'fragment' is included - that's how react reads the empty braces <framgment> sames ad using <> and </>
+    //in this JSX we cannot use conditional expression except when we use { } and turenary statement
     <>
       <h1>Lists</h1>
+      {items.length === 0 ? <p>Not item found</p> : null}{" "}
+      {/* if this message is true return <p> msg otherwise render nothing */}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
